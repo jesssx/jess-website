a7ff52b729468cb10ff75c7c08bb4687e6983692
Jessica <jessica@DESKTOP-OF60KE0.localdomain>
2022-08-16 21:30:02 -0700

    assset update

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -14,14 +14,10 @@ class Contact extends Component {
         <div className='contact-background'>
           <div className='contact'>
             <img src={table} className="table" alt="table"/>
-            <img src={card} className="card" alt="card"/>
             <div className='card-container'>
-              <div className='card-text'>Thanks for visiting!</div>
-              <div className='card-text'>Stay in touch :)</div>
-              <div className='contact-buttons'>
-                <a href='mailto:jjx@mit.edu' target='_blank' className='contact-link'>email</a>
-                <a href='https://www.linkedin.com/in/jessica-xu-145a5821b/' target='_blank' className='contact-link'>linkedin</a>
-              </div>
+              <img src={card} className="card" alt="card"/>
+              <a href='mailto:jjx@mit.edu' target='_blank' className='contact-link'>email</a>
+              <a href='https://www.linkedin.com/in/jessica-xu-145a5821b/' target='_blank' className='contact-link'>linkedin</a>
             </div>
             <Navbar/>
           </div>
