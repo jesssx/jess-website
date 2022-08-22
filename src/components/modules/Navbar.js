242d29c15fe1a5ef290a835a41ba0107529af04c
Jessica <jessica@DESKTOP-OF60KE0.localdomain>
2022-08-22 02:10:29 -0700

    start mobile

--- src/components/modules/Navbar.js
+++ src/components/modules/Navbar.js
@@ -1,29 +1,37 @@
-import React, { Component } from "react";
-// import { Link } from "@reach/router";
+import React, { Component, useState } from "react";
 import "./Navbar.css";
+import { slide as MobileNav } from 'react-burger-menu';
 
 class Navbar extends Component{
     constructor(props) {
         super(props);
     }
     
+    
       render() {
         return (
           <>
             <nav className="nav-container">
-                < a href="/" className="nav-link">HOME</a>
-                < a href="/about" className="nav-link">ABOUT</a>
-                < a href="/art" className="nav-link">ART</a>
-                < a href="/projects" className="nav-link">PROJECTS</a>
-                < a href="/contact" className="nav-link">CONTACT</a>
-                {/* <ul><Link to="/" className="Navbar-link">HOME</Link></ul>
-                <ul><Link to="/about" className="Navbar-link">ABOUT</Link></ul>
-                <ul><Link to="/art" className="Navbar-link">ART</Link></ul>
-                <ul><Link to="/projects" className="Navbar-link">PROJECTS</Link></ul>
-                <ul><Link to="/contact" className="Navbar-link">CONTACT</Link></ul> */}
+                <div className='nav'>
+                  < a href="/" className="nav-link">HOME</a>
+                  < a href="/about" className="nav-link">ABOUT</a>
+                  < a href="/art" className="nav-link">ART</a>
+                  < a href="/projects" className="nav-link">PROJECTS</a>
+                  < a href="/contact" className="nav-link">CONTACT</a>
+                </div>
+                <div className='nav-mobile'>
+                  <MobileNav isOpen={ true } >
+                    < a href="/" className="nav-link-mobile">HOME</a>
+                    < a href="/about" className="nav-link-mobile">ABOUT</a>
+                    < a href="/art" className="nav-link-mobile">ART</a>
+                    < a href="/projects" className="nav-link-mobile">PROJECTS</a>
+                    < a href="/contact" className="nav-link-mobile">CONTACT</a>
+                  </MobileNav>
+                </div>
             </nav>
             </>
         );
       }
     }
+  
 export default Navbar;
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -3,6 +3,7 @@ import './Home.css';
 import Navbar from '../modules/Navbar.js';
 import cafe from '../../assets/cafe.svg';
 import chalkboard from '../../assets/chalkboard.svg';
+import mobileCafe from '../../assets/mobile-cafe.svg';
 
 class Home extends Component {
   constructor(props){
@@ -12,11 +13,12 @@ class Home extends Component {
     return (
       <>
         <div className='home-background'>
-          <Navbar/>
           <div className='home'>
             <img src={cafe} className="cafe" alt="cafe"/>
             <img src={chalkboard} className="chalkboard" alt="chalkboard"/>
+            <img src={mobileCafe} className="mobile-cafe" alt="cafe"/>
           </div>
+          <Navbar/>
         </div>
       </>
     );
